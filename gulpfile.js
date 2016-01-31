aaaf490f77965ceaf9a78babc316c99a07a9a4da
Thomas Hibbard <thomashibbard@Thomass-MacBook-Pro.local>
2016-01-31 15:07:22 -0600

    test with much lighter way to construct scss

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,19 @@ var gulp = require('gulp')
   , cssmin = require('gulp-cssmin')
   , rename = require('gulp-rename');
 
-gulp.task('sass', function() {
-return sass('./css/**/*.scss', { style: 'expanded' })
-  .pipe(gulp.dest('./css/'));
+gulp.task('sass', function(callback) {
+  return sass('./css/**/*.scss', { style: 'expanded' })
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('css/grid.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch-sass', function() {
-  gulp.watch('./css/**/*.scss', ['sass']);
-})
-
-gulp.task('minify-css', function () {
-	gulp.src('lib/**/*.css')
-		.pipe(cssmin())
-		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('css'));
+  gulp.watch('./css/*.scss', ['sass']); 
+  gulp.watch('./css/grid.css', ['minify-css']);
 });
