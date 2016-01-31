abe240c2b1d511bbd42cf8aa25f353995359642c
Thomas Hibbard <thomashibbard@Thomass-MacBook-Pro.local>
2016-01-31 05:54:07 -0600

    added minification task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp')
-  , sass = require('gulp-ruby-sass');
+  , sass = require('gulp-ruby-sass')
+  , cssmin = require('gulp-cssmin')
+  , rename = require('gulp-rename');
 
 gulp.task('sass', function() {
 return sass('./lib/**/*.scss', { style: 'expanded' })
@@ -9,3 +11,10 @@ return sass('./lib/**/*.scss', { style: 'expanded' })
 gulp.task('watch-sass', function() {
   gulp.watch('./lib/**/*.scss', ['sass']);
 })
+
+gulp.task('minify-css', function () {
+	gulp.src('lib/**/*.css')
+		.pipe(cssmin())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest('css'));
+});
