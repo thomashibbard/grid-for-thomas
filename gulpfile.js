b7feaa331becb2606c48db23e2d80cfc2cb17f5f
Thomas Hibbard <loren.hibbard@gmail.com>
2016-01-31 18:11:18 -0600

    switching to add-row-class branch

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,21 @@
 var gulp = require('gulp')
-  , sass = require('gulp-ruby-sass');
+  , sass = require('gulp-ruby-sass')
+  , cssmin = require('gulp-cssmin')
+  , rename = require('gulp-rename');
 
-gulp.task('sass', function() {
-return sass('./lib/**/*.scss', { style: 'expanded' })
-  .pipe(gulp.dest('./lib/'));
+gulp.task('sass', function(callback) {
+  return sass('./css/**/*.scss', { style: 'expanded' })
+    .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('minify-css', function () {
+  return gulp.src('css/grid.css')
+    .pipe(cssmin())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch-sass', function() {
-  gulp.watch('./lib/**/*.scss', ['sass']);
-})
+  gulp.watch('./css/*.scss', ['sass']); 
+  gulp.watch('./css/grid.css', ['minify-css']);
+});
